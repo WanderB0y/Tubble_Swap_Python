918076bcee25dd6c0751568f5f0079bf33bf58d7
Rovic Villaralvo <102729133+kazewanderboy@users.noreply.github.com>
2023-08-25 06:28:00 +0800

    Update TubbleSwap.py

--- TubbleSwap.py
+++ TubbleSwap.py
@@ -10,31 +10,28 @@ print(first_fullname)
 flow = True
 
 while flow:
-
     print("Would you want your first name and last name to change position\n")
     print("Press Y - Yes")
     print("Press N - No\n")
     var_swap = input("Enter Your Choice :: ")
 
     if var_swap == 'Y':
-
         #Changing the value of two variables without using temporary variable
         first_name, last_name = last_name, first_name
         new_fullname = "\n" + "Fullname :: " + first_name.title() + " " + last_name.title() + "\n"
 
-        #Printing it again
+        #Print it again
         print("\nYour fullname has been change")
         print(new_fullname)
-
         #Controlling the loop using boolean
         flow = False
 
     elif var_swap == 'N':
         print("\nYour fullname remains unchange")
         print(first_fullname)
-
         #Controlling the loop using boolean
         flow = False
+
     else:
         print("\nYour input is incorrect please try again\n")
 
